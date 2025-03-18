fa26cfe6e40a03a4c6517278b5bc644bee52dfa0
TheRepeater77 <russchoolacc@gmail.com>
2025-03-18 12:20:14 +0800

    Made Highlight Function

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const ingredients = document.querySelector("#recipe-ingredients").childNodes;
+const ingredients = document.querySelectorAll("#recipe-ingredients li");
 
-ingredients.forEach((e, i) =>{
-    console.log(i % 2);
-    if(i % 2 == 0){
-        e.classList = "highlight";
-    }
+const instructions = document.querySelectorAll("#recipe-instructions li");
+
+const btn_highlight = document.querySelector("#btn_highlight");
+const btn_check = document.querySelector("#btn_check");
+const btn_reset = document.querySelector("#btn_reset");
+
+btn_highlight.addEventListener("click",()=>{
+    highlight();
 });
+btn_check.addEventListener("click",()=>{
+    check();
+});
+btn_reset.addEventListener("click",()=>{
+    reset();
+});
+
+function highlight(){
+    console.log(ingredients);
+    ingredients.forEach((e, i) =>{
+        if(i % 2 == 0){
+            e.classList.toggle("highlight");
+        }
+    });
+}
+
+function check(){
+    console.log(instructions);
+}
