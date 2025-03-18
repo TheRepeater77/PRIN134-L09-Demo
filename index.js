f9cd06fd193ee08dafe658096abb13f0ade58052
TheRepeater77 <124745527+TheRepeater77@users.noreply.github.com>
2025-03-18 22:20:49 +0800

    Added animations

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ const btn_highlight = document.querySelector("#btn_highlight");
 const btn_check = document.querySelector("#btn_check");
 const btn_reset = document.querySelector("#btn_reset");
 
+const headers = document.querySelectorAll(".headers");
+let ingredients_header;
+let instructions_header;
+headers.forEach(e => {
+    switch(e.innerHTML){
+        case "Ingredients":
+            ingredients_header = e;
+            break;
+        case "Instructions":
+            instructions_header = e;
+            break;
+        default:
+            break;
+    }
+});
+
+const cake = document.querySelector("#cake");
+
 btn_highlight.addEventListener("click",()=>{
     highlight();
 });
@@ -51,11 +69,32 @@ setInterval(() => {
         time_interval = 3000;
     }
 }, 65);
-/*  CHECK END =========================================================================== */
+/* CHECK END =========================================================================== */
 
+
+/* =========================================================================== RESET START */
 function reset(){
-    instructions.forEach((e,i) => {
-        e.classList = 0;
+    instructions.forEach((e) => {
+        e.classList = "";
     });
     check_bool = false;
 }
+/* RESET END =========================================================================== */
+
+/* =========================================================================== ANIMATIONS START */
+ingredients_header.addEventListener("click",()=>{
+    ingredients_header.style.animation = "none"
+    ingredients_header.offsetHeight;
+    ingredients_header.style.animation = "bounce 0.5s"
+});
+instructions_header.addEventListener("click",()=>{
+    instructions_header.style.animation = "none"
+    instructions_header.offsetHeight;
+    instructions_header.style.animation = "rubberBand 0.5s"
+});
+cake.addEventListener("click",()=>{
+    cake.style.animation = "none"
+    cake.offsetHeight;
+    cake.style.animation = "rotateIn 0.5s"
+});
+/* ANIMATIONS END =========================================================================== */
